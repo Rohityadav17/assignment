41a5753b6cf9a0bd084f9d56ebb26db5f0666138
Rohityadav17 <rohityadavthe007@gmail.com>
2023-11-08 08:05:39 +0530

    hystrix

--- ecom/src/main/java/com/example/ecom/service/WeatherService.java
+++ ecom/src/main/java/com/example/ecom/service/WeatherService.java
@@ -6,18 +6,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-//import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;
 import com.example.ecom.pojo.WeatherData;
+import com.example.ecom.pojo.WeatherResponse;
 
 
 @Service
 public class WeatherService {
 
-	//@Autowired
-   // private RestTemplate restTemplate;
-
 	@Autowired
-	FeignClientService feignService;
+   private RestTemplate restTemplate;
+
+	//@Autowired
+	//FeignClientService feignService;
 	
 
     @Value("${weather.service.api-key}")
@@ -26,8 +27,9 @@ public class WeatherService {
 
 
     public List<WeatherData> getWeatherForecastFor30Days( String city) { 
-    	//String apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city  + "&appid=" + apiKey;
-    	List<WeatherData> forecast = feignService.getWeatherForecast(city,apiKey).getList();
+    	String apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city  + "&appid=" + apiKey;
+    	List<WeatherData> forecast = restTemplate.getForObject(apiUrl, WeatherResponse.class).getList();
+    	//List<WeatherData> forecast = feignService.getWeatherForecast(city,apiKey).getList();
 
         if (forecast != null) {
             return forecast;
