aae1af4ff3c4155327b14b06a2499b9fcba74d54
Rohityadav17 <rohityadavthe007@gmail.com>
2023-12-07 08:06:48 +0530

    without *ngFor

--- My-Carousel/src/app/carousel/carousel.component.ts
+++ My-Carousel/src/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -6,12 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
-  images: string[] = ['https://images.pexels.com/photos/9992332/pexels-photo-9992332.jpeg?auto=compress&cs=tinysrgb&w=600','https://images.pexels.com/photos/9992330/pexels-photo-9992330.jpeg?auto=compress&cs=tinysrgb&w=600','https://images.pexels.com/photos/9992435/pexels-photo-9992435.jpeg?auto=compress&cs=tinysrgb&w=600','https://images.pexels.com/photos/4676400/pexels-photo-4676400.jpeg?auto=compress&cs=tinysrgb&w=600']; 
+  @Input() images: string[] = ['https://images.pexels.com/photos/9992332/pexels-photo-9992332.jpeg?auto=compress&cs=tinysrgb&w=600','https://images.pexels.com/photos/9992330/pexels-photo-9992330.jpeg?auto=compress&cs=tinysrgb&w=600','https://images.pexels.com/photos/9992435/pexels-photo-9992435.jpeg?auto=compress&cs=tinysrgb&w=600','https://images.pexels.com/photos/4676400/pexels-photo-4676400.jpeg?auto=compress&cs=tinysrgb&w=600']; 
   currentIndex = 0;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  getPreviousImage(): string {
+    return this.images[(this.currentIndex - 1 + this.images.length) % this.images.length];
+  }
+
+  getNextImage(): string {
+    return this.images[(this.currentIndex + 1) % this.images.length];
+  }
 
   prev(): void {
     this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
@@ -21,3 +29,7 @@ export class CarouselComponent implements OnInit {
     this.currentIndex = (this.currentIndex + 1) % this.images.length;
   }
 }
+
+
+
+
